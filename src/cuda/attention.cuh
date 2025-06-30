d1f328e4cd2235cd70d80de0b981dc30e098c1b8
onenewcode <lspneverstudy@gmail.com>
2025-06-30 09:15:44 +0800

    格式化文档

--- src/cuda/attention.cuh
+++ src/cuda/attention.cuh
@@ -11,12 +11,12 @@ struct kv_cache {
 template <typename T>
 __device__ kv_cache<T> locate_cache(
     T *const *pages,
-    int64_t  sbuf,      // sequence stride
-    int64_t  skv,       //   k to v stride
-    int64_t  sh,        //  kv head stride
-    uint64_t  const bs, // context tile
-    uint64_t  const head,
-    uint64_t  const pos) {
+    int64_t sbuf,      // sequence stride
+    int64_t skv,       //   k to v stride
+    int64_t sh,        //  kv head stride
+    uint64_t const bs, // context tile
+    uint64_t const head,
+    uint64_t const pos) {
     sh *= head;
     sbuf *= pos % bs;
     uint8_t *page = (uint8_t *)pages[pos / bs];
@@ -31,21 +31,21 @@ __device__ void __attn(
     bool const *mask_,     // n x s
     T *m,                  // s
     T *l,                  // s
-    uint64_t  const n,      // sequence length
-    uint64_t  const d,      // head dim
-    uint64_t  const ts,     // = s/bs
-    uint64_t  const bs,     // context tile
-    int64_t  const sq,      //        q stride
-    int64_t  const so,      //        o stride
-    int64_t  const kv_sbuf, // sequence stride
-    int64_t  const kv_skv,  //   k to v stride
-    int64_t  const kv_sh,   //  kv head stride
+    uint64_t const n,      // sequence length
+    uint64_t const d,      // head dim
+    uint64_t const ts,     // = s/bs
+    uint64_t const bs,     // context tile
+    int64_t const sq,      //        q stride
+    int64_t const so,      //        o stride
+    int64_t const kv_sbuf, // sequence stride
+    int64_t const kv_skv,  //   k to v stride
+    int64_t const kv_sh,   //  kv head stride
     float const scale) {
     // (batch x head) x (bn)
-    uint64_t  const head = blockIdx.x;
-    uint64_t  const bn = blockDim.x;
-    uint64_t  const it = threadIdx.x;
-    uint64_t  const tn = (n + bn - 1) / bn;
+    uint64_t const head = blockIdx.x;
+    uint64_t const bn = blockDim.x;
+    uint64_t const it = threadIdx.x;
+    uint64_t const tn = (n + bn - 1) / bn;
 
     extern __shared__ T sram[];
     int tile_size = bs * d;
@@ -54,23 +54,23 @@ __device__ void __attn(
     T *vj = &sram[tile_size * 2];
     T *x = &sram[tile_size * 3];
     // kv
-    for (uint64_t  ikvb = 0; ikvb < ts; ++ikvb) {
+    for (uint64_t ikvb = 0; ikvb < ts; ++ikvb) {
         // 加载kv
         { // 每个线程拷贝 k/v 的一行，拷贝整个 kv block 到 local memory
-            uint64_t  const end = (ikvb + 1) * bs;
-            for (uint64_t  ikv = ikvb * bs + it, i = it; ikv < end; ikv += bn, i += bn) {
+            uint64_t const end = (ikvb + 1) * bs;
+            for (uint64_t ikv = ikvb * bs + it, i = it; ikv < end; ikv += bn, i += bn) {
                 kv_cache const cache = locate_cache(kv_pages, kv_sbuf, kv_skv, kv_sh, bs, head, ikv);
-                for (uint64_t  j = 0; j < d; ++j) {
+                for (uint64_t j = 0; j < d; ++j) {
                     kj[i * d + j] = cache.k[j];
-                    vj[i * d + j] =cache.v[j];
+                    vj[i * d + j] = cache.v[j];
                 }
             }
             __syncthreads();
         }
         { // 每个线程计算 q 的一行
 
-            for (uint64_t  iqb = 0; iqb < tn; ++iqb) {
-                uint64_t  iq = iqb * bn + it;
+            for (uint64_t iqb = 0; iqb < tn; ++iqb) {
+                uint64_t iq = iqb * bn + it;
                 if (iq >= n) {
                     break;
                 }
@@ -79,7 +79,7 @@ __device__ void __attn(
                 T *o = o_ + iq * so;
                 bool const *mask = mask_ + iq * n + ikvb * bs;
                 // load data
-                for (uint64_t  i = 0; i < d; ++i) {
+                for (uint64_t i = 0; i < d; ++i) {
                     qi[i] = q[i];
                 }
 
@@ -88,13 +88,13 @@ __device__ void __attn(
 
                 // score = q @ k^T / √d
                 T mi = mi_1;
-                for (uint64_t  i = 0; i < bs; ++i) {
+                for (uint64_t i = 0; i < bs; ++i) {
                     if (!mask[i]) {
                         x[i] = -CUDART_INF_F;
                     } else {
                         T const *k = kj + i * d;
 
-                        for (uint64_t  j = 0; j < d; ++j) {
+                        for (uint64_t j = 0; j < d; ++j) {
                             x[i] += qi[j] * kj[j];
                         }
                         x[i] *= scale;
@@ -106,7 +106,7 @@ __device__ void __attn(
                 }
                 // P = exp(S - row_m), row_l = rowsum(P)
                 T sum = 0;
-                for (uint64_t  i = 0; i < bs; ++i) {
+                for (uint64_t i = 0; i < bs; ++i) {
                     x[i] = ::exp(x[i] - mi);
                     sum += x[i];
                 }
@@ -119,8 +119,22 @@ __device__ void __attn(
 
                 T rdi = 1 / di;
                 exp *= rdi;
-                for (uint64_t  i = 0; i < bs; ++i) {
+                for (uint64_t i = 0; i < bs; ++i) {
                     x[i] *= rdi;
+                }
+                T *xv = new T[d];
+                for (uint64_t i = 0; i < d; ++i) {
+                    xv[i] = 0;
+                }
+                for (uint64_t i = 0; i < bs; ++i) {
+                    T xi = x[i];
+                    T *vji = &vj[i * d];
+                    for (uint64_t j = 0; j < d; ++j) {
+                        xv[j] += xi * vji[j];
+                    }
+                }
+                for (uint64_t j = 0; j < d; ++j) {
+                    o[j] = o[j] * exp + xv[j];
                 }
             }
             __syncthreads();
@@ -134,15 +148,15 @@ extern "C" __global__ void __attn_f64(
     bool const *mask_,
     double *m,
     double *l,
-    uint64_t  const n,
-    uint64_t  const d,
-    uint64_t  const ts,
-    uint64_t  const bs,
-    int64_t  const sq,
-    int64_t  const so,
-    int64_t  const kv_sbuf,
-    int64_t  const kv_skv,
-    int64_t  const kv_sh,
+    uint64_t const n,
+    uint64_t const d,
+    uint64_t const ts,
+    uint64_t const bs,
+    int64_t const sq,
+    int64_t const so,
+    int64_t const kv_sbuf,
+    int64_t const kv_skv,
+    int64_t const kv_sh,
     float const scale) {
     // 调用模板实现
     __attn<double>(kv_pages, q_, o_, mask_, m, l, n, d, ts, bs, sq, so, kv_sbuf, kv_skv, kv_sh, scale);
